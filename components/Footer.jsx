f87e8bd1e858b3137311f5bfbd8a542270653e64
markotomic <markotomic.dev@gmail.com>
2023-01-08 10:44:41 +0100

    Update January 8, 2023

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,19 +9,14 @@ const Footer = () => {
         <div className="FooterBox max-w-[1280px] w-full py-[30px] ">
           <div className="FooterGrid grid grid-cols-1 md:grid-cols-2 gap-[50px] md:gap-[100px] w-full">
             <div className="BrandAbout flex flex-col">
-              <h1 className="text-3xl text-froggy-green font-bold mb-[20px]">
-                Marko Tomic
-              </h1>
+              <h1 className="text-3xl text-froggy-green font-bold mb-[20px]">Marko Tomic</h1>
               <p className="text-[#e6e6e6]">
-                IT Engineer & Frontend Developer with broad experience
-                developing fast and powerfull websites.
+                IT Engineer & Frontend Developer with broad experience developing fast and powerfull websites.
               </p>
             </div>
             <div className="grid grid-cols-2 gap-[50px]">
               <div className="Links">
-                <h3 className="text-3xl text-froggy-green font-bold mb-[20px]">
-                  About Me
-                </h3>
+                <h3 className="text-3xl text-froggy-green font-bold mb-[20px]">About Me</h3>
                 <div className="flex flex-col text-[#e6e6e6] gap-[5px]">
                   <div>
                     <Link href="/">Home</Link>
@@ -35,9 +30,7 @@ const Footer = () => {
                 </div>
               </div>
               <div className="Socials">
-                <h3 className="text-3xl text-froggy-green font-bold mb-[20px]">
-                  Follow Me
-                </h3>
+                <h3 className="text-3xl text-froggy-green font-bold mb-[20px]">Follow Me</h3>
                 <div className="flex gap-[15px] text-[#e6e6e6]">
                   <a
                     href="https://www.linkedin.com/in/marko-tomic-web-developer/"
@@ -70,7 +63,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="FooterLine text-froggy-green flex justify-center bg-[#222222] w-full py-[20px]">
-        Copyright © 2022 Marko Tomic. All rights reserved.
+        Copyright © 2023 Marko Tomic. All rights reserved.
       </div>
     </div>
   );
